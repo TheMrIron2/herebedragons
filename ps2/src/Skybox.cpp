775756f3f17b89c2af2d663620ada9ba24b27288
TheMrIron2 <dwelch.appleid@gmail.com>
2017-12-31 12:52:19 +0000

    Update Skybox.cpp

--- ps2/src/Skybox.cpp
+++ ps2/src/Skybox.cpp
@@ -24,7 +24,7 @@ Skybox::Skybox(){
 	prim.shading = PRIM_SHADE_GOURAUD;
 	prim.mapping = DRAW_ENABLE;
 	prim.fogging = DRAW_DISABLE;
-	prim.blending = DRAW_ENABLE;
+	prim.blending = DRAW_DISABLE;
 	prim.antialiasing = DRAW_DISABLE;
 	prim.mapping_type = PRIM_MAP_ST;
 	prim.colorfix = PRIM_UNFIXED;
@@ -65,7 +65,7 @@ void Skybox::render(packet_t * packet, packet_t * texturePacket, MATRIX world_vi
 	// Load the texture into vram.
 	qword_t * qt = texturePacket->data;
 	
-	qt = draw_texture_transfer(qt, _texture, 256, 256, GS_PSM_24, _texbuf->address, _texbuf->width);
+	qt = draw_texture_transfer(qt, _texture, 384, 384, GS_PSM_8, _texbuf->address, _texbuf->width);
 	
 	qt = draw_texture_flush(qt);
 	dma_channel_send_chain(DMA_CHANNEL_GIF, texturePacket->data, qt - texturePacket->data, 0,0);
